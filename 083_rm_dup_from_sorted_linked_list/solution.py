d211af4940c3f50be6e4ee3873181e69f3efa821
wushuzh <zhangjiaqiang@gmail.com>
2018-07-22 16:44:56 +0800

    Adjust with more clear conditions

--- 083_rm_dup_from_sorted_linked_list/solution.py
+++ 083_rm_dup_from_sorted_linked_list/solution.py
@@ -9,8 +9,9 @@ class ListNode:
 
 def deleteDuplicates(head: ListNode) -> ListNode:
     cur = head
-    while cur:
-        while cur.next and cur.val == cur.next.val:
+    while cur and cur.next:
+        if cur.next.val == cur.val:
             cur.next = cur.next.next
-        cur = cur.next
+        else:
+            cur = cur.next
     return head

7b1f65d5f5114b31c94628d072abbabdd133b070
wushuzh <zhangjiaqiang@gmail.com>
2018-08-07 14:37:57 +0800

    Add hypothesis test cases

--- slicing_a_list_evenly/test_solution.py
+++ slicing_a_list_evenly/test_solution.py
@@ -1,6 +1,10 @@
 import pytest
+import hypothesis as ht
+import hypothesis.strategies as st
+import functools
 
-from solution import chunk
+
+from solution import chunk, chunk_iteratools
 
 
 def test_chunk():
@@ -16,3 +20,32 @@ def test_chunk():
 
     rs = range(1000000)
     assert chunk(rs, 2) == [range(500000), range(500000, 1000000)]
+
+
+@st.composite
+def items_and_chunk_count(draw):
+    xs = draw(st.one_of(st.text(),
+                        st.binary(),
+                        st.lists(st.integers())))
+    n = draw(st.integers(min_value=1,
+                         max_value=max(1, len(xs))))
+    return xs, n
+
+
+@ht.given(xs_n=items_and_chunk_count())
+def test_evenly_chunked(xs_n):
+    '''Verify there are n evenly sized chunks'''
+    xs, n = xs_n
+    chunks = chunk_iteratools(xs, n)
+    assert len(chunks) == n
+    chunk_lens = {len(c) for c in chunks}
+    assert len(chunk_lens) in {1, 2}
+    assert max(chunk_lens) - min(chunk_lens) in {0, 1}
+
+
+@ht.given(xs_n=items_and_chunk_count())
+def test_combining_chunks(xs_n):
+    '''Verify recombining the chunks reproduces the original sequence.'''
+    xs, n = xs_n
+    chunks = chunk_iteratools(xs, n)
+    assert functools.reduce(lambda x, y: x+y, chunks) == xs

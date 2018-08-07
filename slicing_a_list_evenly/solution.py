6705e92438b0c4c5642a4502d0d75f47c69288ac
wushuzh <zhangjiaqiang@gmail.com>
2018-08-07 14:32:29 +0800

    Add itertools solution for slicing_a_list_evenly

--- slicing_a_list_evenly/solution.py
+++ slicing_a_list_evenly/solution.py
@@ -1,3 +1,6 @@
+from itertools import accumulate, chain, repeat, tee
+
+
 def chunk(xs, n):
     '''Split the list, xs, into n chunks
     '''
@@ -7,3 +10,15 @@ def chunk(xs, n):
     t = s + 1
     return ([xs[p:p+t] for p in range(0, r*t, t)] +
             [xs[p:p+s] for p in range(r*t, L, s)])
+
+
+def chunk_iteratools(xs, n):
+    '''Split the list, xs, into n evenly sized chunks'''
+    assert n > 0
+    L = len(xs)
+    s, r = divmod(L, n)
+    widths = chain(repeat(s+1, r), repeat(s, n-r))
+    offsets = accumulate(chain((0,), widths))
+    b, e = tee(offsets)
+    next(e)
+    return [xs[s] for s in map(slice, b, e)]

943cd11bebc6c23dc9c1a25edb695ede32882b2e
wushuzh <zhangjiaqiang@gmail.com>
2019-04-02 12:44:06 +0800

    Fix bug so given arg of jewels is distinct
    Add one liner implementations
    In case jewels is not distinct, the v3 and v4 will count wrongly
    e.g., num_jewels_in_stones_v3/4('00', '0') returns 2 instead of 1
    distinct is a must to use map and count

--- 771_jewels_and_stones/solution.py
+++ 771_jewels_and_stones/solution.py
@@ -4,3 +4,15 @@ def num_jewels_in_stones_v1(J: str, S: str) -> int:
         if c in J:
             count += 1
     return count
+
+
+def num_jewels_in_stones_v2(J, S):
+    return sum(s in J for s in S)
+
+
+def num_jewels_in_stones_v3(J, S):
+    return sum(map(J.count, S))
+
+
+def num_jewels_in_stones_v4(J, S):
+    return sum(map(S.count, J))

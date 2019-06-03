77d0ef318cce5097165eefadf5c14c4f3448336c
wushuzh <zhangjiaqiang@gmail.com>
2019-06-03 16:24:38 +0800

    Add leetcode-162 recursive and iterative solution
    These two methods are based on same idea but different forms, they are
    better than v1 since more compact since they takes left and right index
    of highlighted range in each round comparing with v1

--- 162_find_peak_element/solution.py
+++ 162_find_peak_element/solution.py
@@ -15,3 +15,27 @@ def find_peak_element_v1(nums: List[int]) -> int:
         return find_peak_element_v1(nums[:m])
     elif nums[m] < nums[m + 1]:
         return m + 1 + find_peak_element_v1(nums[m + 1:])
+
+
+def find_peak_element_v2(nums: List[int]) -> int:
+    def search(nums: List[int], l: int, r: int) -> int:
+        if (l == r):
+            return l
+        m = (l + r) // 2
+        if (nums[m] > nums[m + 1]):
+            return search(nums, l, m)
+        return search(nums, m + 1, r)
+
+    return search(nums, 0, len(nums) - 1)
+
+
+def find_peak_element_v3(nums: List[int]) -> int:
+    l: int = 0
+    r: int = len(nums) - 1
+    while (l < r):
+        m = (l + r) // 2
+        if (nums[m] < nums[m + 1]):
+            l = m + 1
+        else:
+            r = m
+    return l

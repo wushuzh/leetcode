0e9afaab7f10bc77441e0fbaf99e5ac75d1b977b
wushuzh <zhangjiaqiang@gmail.com>
2019-06-03 16:24:38 +0800

    Add leetcode-162 recursive and iterative solution
    These two methods are based on same idea but different forms, they are
    better than v1 since more compact since they takes left and right index
    of highlighted range in each round comparing with v1

--- 162_find_peak_element/test_solution.py
+++ 162_find_peak_element/test_solution.py
@@ -1,25 +1,37 @@
-from solution import find_peak_element_v1
+from solution import find_peak_element_v1, find_peak_element_v2, find_peak_element_v3
 
 
 def test_single_element():
     assert 0 == find_peak_element_v1([1])
+    assert 0 == find_peak_element_v2([1])
+    assert 0 == find_peak_element_v3([1])
 
 
 def test_two_element_later_peak():
     assert 1 == find_peak_element_v1([0, 1])
+    assert 1 == find_peak_element_v2([0, 1])
+    assert 1 == find_peak_element_v3([0, 1])
 
 
 def test_two_element_first_peak():
     assert 0 == find_peak_element_v1([1, 0])
+    assert 0 == find_peak_element_v2([1, 0])
+    assert 0 == find_peak_element_v3([1, 0])
 
 
 def test_3_elements_first_peak():
     assert 0 == find_peak_element_v1([3, 2, 1])
+    assert 0 == find_peak_element_v2([3, 2, 1])
+    assert 0 == find_peak_element_v3([3, 2, 1])
 
 
 def test_3_elements_mid_peak():
     assert 1 == find_peak_element_v1([1, 3, 2])
+    assert 1 == find_peak_element_v2([1, 3, 2])
+    assert 1 == find_peak_element_v3([1, 3, 2])
 
 
 def test_3_elements_last_peak():
     assert 2 == find_peak_element_v1([1, 2, 3])
+    assert 2 == find_peak_element_v2([1, 2, 3])
+    assert 2 == find_peak_element_v3([1, 2, 3])

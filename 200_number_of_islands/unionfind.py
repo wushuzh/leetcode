f5dea514356e633f16ef4471c63a2677692675b7
wushuzh <zhangjiaqiang@gmail.com>
2019-06-11 09:25:43 +0800

    Add weight to union-find for balance

--- 200_number_of_islands/unionfind.py
+++ 200_number_of_islands/unionfind.py
@@ -1,8 +1,9 @@
 class UnionFind:
     def __init__(self, grid):
         self.count = 0
-        r, c = len(grid), len(grid[0])
+        self.r, self.c = r, c = len(grid), len(grid[0])
         self.parent = [-1] * (r * c)
+        self.weight = [0] * (r * c)
         for i in range(r):
             for j in range(c):
                 if grid[i][j] == '1':
@@ -18,8 +19,20 @@ class UnionFind:
         proot = self.find_root(p)
         qroot = self.find_root(q)
         if proot != qroot:
-            self.parent[proot] = qroot
+            if self.weight[proot] > self.weight[qroot]:
+                self.parent[proot] = qroot
+            elif self.weight[proot] < self.weight[qroot]:
+                self.parent[qroot] = proot
+            else:
+                self.parent[proot] = qroot
+                self.weight[q] += 1
             self.count -= 1
 
     def __str__(self):
-        return 'TODO: show internal data for debug'
+        """TODO: visualization for debug and performance checking
+        """
+        roots = []
+        for i in range(self.r * self.c):
+            if self.parent[i] == i:
+                roots.append("root: %s, %s" % (i // self.c, i % self.c))
+        return "%s\ncount: %s\n" % ('\n'.join(roots), self.count)

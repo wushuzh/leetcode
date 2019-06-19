83c52b80fbb4cc6cbeee2a508af3f5e7e85fd935
wushuzh <zhangjiaqiang@gmail.com>
2019-06-19 17:26:25 +0800

    Add leetcode-947 using simpler union-find

--- 947_most_stones_rm_with_same_row_or_col/solution.py
+++ 947_most_stones_rm_with_same_row_or_col/solution.py
@@ -13,3 +13,11 @@ def rm_stones_using_union_find(stones: List[List[int]]) -> int:
                 uf.union(10000 * x + y, 10000 * i + j)
 
     return len(uf.p) - len({uf.find_root(10000 * x + y) for x, y in stones})
+
+
+def rm_stones_using_union_find_simple_view(stones):
+    uf = UnionFind()
+    for x, y in stones:
+        uf.union(x, 10000 + y)
+
+    return len(stones) - len({uf.find_root(i) for i, _ in stones})
